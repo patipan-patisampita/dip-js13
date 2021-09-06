4c5db4e53d5f0c990f467b20723fad930c382325
patipan-patisampita <patipan.patisampita@gmail.com>
2021-09-06 15:27:48 +0700

    initial commit

---
+++ first.js
@@ -0,0 +1,13 @@
+function writeSomeText() {
+    document.getElementById("demo").innerHTML = "This is My Family";
+    document.getElementById("demo2").innerHTML = "This is a Fish";
+}
+writeSomeText();
+
+/* var myname = "Elon Mask";
+var myName = "Mask Sakaberg";
+var first_name = "Jack Ma";
+console.log(myname);
+console.log(myName);*/
+
+//console.log(first_name);
---
+++ second.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+alert(`${firstName} ${lastName}`);
+document.getElementById("demo").innerHTML = `Hi ${firstName} ${lastName}`;
+console.log(`Hi ${firstName} ${lastName} ! `);
